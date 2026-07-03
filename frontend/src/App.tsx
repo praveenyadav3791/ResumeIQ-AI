901ee2703c2c7e04b46f23dd52029477a3162b4a
Praveen Yadav <praveen379161@gmail.com>
2026-07-04 01:42:33 +0530

    project stucture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
 function App() {
   return (
     <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <div className="bg-white p-10 rounded-2xl shadow-2xl">
-        <h1 className="text-4xl font-bold text-blue-600">
-          ResumeIQ AI 🚀
-        </h1>
-
-        <p className="mt-4 text-gray-600">
-          AI Powered Resume Optimizer
-        </p>
-
-        <button className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
-          Upload Resume
-        </button>
-      </div>
+      <h1 className="text-5xl font-bold text-white">
+        ResumeIQ AI 🚀
+      </h1>
     </div>
   );
 }

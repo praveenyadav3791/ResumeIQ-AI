83c03c7fea90beb628a5cb8588ac33543f497c4e
Praveen Yadav <praveen379161@gmail.com>
2026-07-04 00:15:47 +0530

    Intitail React and Tailwind setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-2xl">
+        <h1 className="text-4xl font-bold text-blue-600">
+          ResumeIQ AI 🚀
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          AI Powered Resume Optimizer
+        </p>
+
+        <button className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
+          Upload Resume
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
